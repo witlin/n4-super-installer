7db67a2322494ce869e067db79ca62e345000675
vitek1 <wtsmolinski@gmail.com>
2020-12-03 20:14:40 -0500

    finished product, needs full functional testing

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/readme/readme.css">
+
 # n4-super-installer
-It automates portions of the installation process for an N4 Supervisor
+This powershell script automates portions of the installation process for an N4 Supervisor.
+
+## Directions of use
+1. Follow the Install guide up to step 3, **don't create** the *D:\Controls Software*
+![installation guide step 3](assets/readme/step3.png)
+
+2. Copy and paste the **n4-super-installer** folder from your computer to the server instead.
+
+3. Open the Windows Start Menu and type *powershell*, then select Run As Administrator
+![start menu powershell selection](assets/readme/start-menu-ps.png)
+
+4. Type
+``` 
+cd "<PATH TO FOLDER>\n4-super-installer\scripts" 
+```
+
+5. Press **Enter** and type
+```
+Set-ExecutionPolicy Bypass -Scope Process -Force; .\scripts\installer.ps1
+```
+
+6. Press **Enter** and wait to follow the prompts for the GFX installation setup window, the other software applications will be installed in sequential order. At this point the installer has created the *D:\Controls Software* folder and copied all the contents from the current download folder to where applications should be in.
+
+![gfx setup window](assets/readme/gfx-setup-window.png)
+<br><br>
+
+> In eventvwr.msc, we are looking for the process-creation (4688) and process-terminations (4689) events in the Windows event viewer to track the overall install process
+
+<br>The expected output in the event viewer for each setup.exe program this installer runs should look like this:
+| Keywords      | Date and Time         | Source                               | Event ID | Task Category    |
+| ------------- | --------------------- | ------------------------------------ | -------- | ---------------- |
+| Audit Success	| 12/1/2020 12:44:21 PM | Microsoft Windows security auditing. | 4688     |	Process Creation |
+| Audit Success	| 12/1/2020 12:44:21 PM | Microsoft Windows security auditing. | 4688     | Process Creation |
+<br>
+followed by:<br>
+
+| Keywords      | Date and Time         | Source                               | Event ID | Task Category       |
+| ------------- | --------------------- | ------------------------------------ | -------- | ------------------- |
+| Audit Success	| 12/1/2020 12:48:37 PM | Microsoft Windows security auditing. | 4688     |	Process Termination |
+| Audit Success	| 12/1/2020 12:49:12 PM | Microsoft Windows security auditing. | 4688     | Process Termination |
+
+**full event log details**
+```
+Log Name:      Security
+Source:        Microsoft-Windows-Security-Auditing
+Date:          12/1/2020 12:44:21 PM
+Event ID:      4688
+Task Category: Process Creation
+Level:         Information
+Keywords:      Audit Success
+User:          N/A
+Computer:      WinDev2010Eval
+Description:
+A new process has been created.
+
+Creator Subject:
+	Security ID:		S-1-5-21-3858184157-4174427647-2357676267-1001
+	Account Name:		User
+	Account Domain:		WINDEV2010EVAL
+	Logon ID:		0x31F70
+
+Target Subject:
+	Security ID:		S-1-0-0
+	Account Name:		-
+	Account Domain:		-
+	Logon ID:		0x0
+
+Process Information:
+	New Process ID:		0x249c
+	New Process Name:	C:\Users\User\AppData\Local\Temp\is-NVBLR.tmp\Distech Controls EC-gfxProgram v6.6.20212 Setup.tmp
+	Token Elevation Type:	%%1937
+	Mandatory Label:		S-1-16-12288
+	Creator Process ID:	0x26c0
+	Creator Process Name:	C:\Users\User\projects\n4-super-installer\assets\installers\Distech Controls EC-gfxProgram v6.6.20212 Setup.exe
+```
+<br>
+
+```
+Log Name:      Security
+Source:        Microsoft-Windows-Security-Auditing
+Date:          12/1/2020 12:44:21 PM
+Event ID:      4688
+Task Category: Process Creation
+Level:         Information
+Keywords:      Audit Success
+User:          N/A
+Computer:      WinDev2010Eval
+Description:
+A new process has been created.
+
+Creator Subject:
+	Security ID:		S-1-5-21-3858184157-4174427647-2357676267-1001
+	Account Name:		User
+	Account Domain:		WINDEV2010EVAL
+	Logon ID:		0x31F70
+
+Target Subject:
+	Security ID:		S-1-0-0
+	Account Name:		-
+	Account Domain:		-
+	Logon ID:		0x0
+
+Process Information:
+	New Process ID:		0x26c0
+	New Process Name:	C:\Users\User\projects\n4-super-installer\assets\installers\Distech Controls EC-gfxProgram v6.6.20212 Setup.exe
+	Token Elevation Type:	%%1937
+	Mandatory Label:		S-1-16-12288
+	Creator Process ID:	0x6fc
+	Creator Process Name:	C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+```
+7. Run through all the windows for all the software installers, once the installer script is finished you should be ready to commission the new EC-NET instance.
+
+> Command References   
+  secpol.msc<br>
+  eventvwr.msc <br>
+  clear-eventlog -logname Security -computername ($env:COMPUTERNAME)  
---
+++ scripts/installer.ps1
@@ -0,0 +1,156 @@
+<#
+
+#>
+
+# variables and constants
+$errorBase = "[ERROR] " + (Get-Date -Format "MM/dd/yyyy HH:mm:ss")
+$infoBase = "[INFO] " + (Get-Date -Format "MM/dd/yyyy HH:mm:ss")
+$ctrlDir = "D:\Controls Software"
+$setupPaths = Get-ChildItem -Path .\assets\installers -Filter *.exe 
+
+# functions
+function log-step {
+  param (
+    [parameter (Mandatory=$true)]
+    [string] $msg
+  )
+  Write-Host $msg
+}
+function audit-process {
+  param (
+    [Parameter(Mandatory=$false)]
+     [bool] $enable = $true
+    )
+  if ($enable) {
+    if (Test-Path -Path C:\secpol.cfg){
+      Remove-Item -Path C:\secpol.cfg -Force
+      SecEdit.exe /export /cfg C:\secpol.cfg
+      (Get-Content -Path C:\secpol.cfg).Replace("AuditProcessTracking = 0", "AuditProcessTracking = 1") | 
+        Out-File C:\secpol.cfg
+      SecEdit.exe /configure /db C:\Windows\security\local.sdb /cfg C:\secpol.cfg /areas SECURITYPOLICY
+      Remove-Item -Force -Path C:\secpol.cfg
+    } else {
+      SecEdit.exe /export /cfg C:\secpol.cfg
+      (Get-Content -Path C:\secpol.cfg).Replace("AuditProcessTracking = 0", "AuditProcessTracking = 1") | 
+        Out-File C:\secpol.cfg
+      SecEdit.exe /configure /db C:\Windows\security\local.sdb /cfg C:\secpol.cfg /areas SECURITYPOLICY
+      Remove-Item -Force -Path C:\secpol.cfg
+    }
+  }
+}
+function run-setup {
+  param (
+    [Parameter(Mandatory=$true)] [string] $setupPath,
+    [Parameter(Mandatory=$true)] [string] $filterExpression
+  )
+  start-process -filepath $setupPath
+  Start-Sleep -s 2
+  $proc = get-process -ProcessName $filterExpression
+  log-step -msg $infoBase" Scanning for the setup process"
+  Write-Host "Process ID found: "$proc[0].Id
+
+  while ($proc.Length -gt 0) {
+    Start-Sleep -Seconds 5
+    $proc = get-process -ProcessName $filterExpression
+  }
+  
+  Start-Sleep -s 1
+  $evEnd = Get-EventLog -LogName Security -InstanceId 4689 -Newest 1
+  $repStrs = $evEnd[0] | Select-Object -Property ReplacementStrings
+  $processID = [convert]::ToInt64($repStrs.ReplacementStrings[5], 16)
+  $processName = $repStrs.ReplacementStrings[6]
+  log-step -msg $infoBase" Windows Event verification"
+  Write-Host "Name: "$processName
+  Write-Host "PID: "$processID
+}
+
+# LOGIC
+if (Test-Path -Path $ctrlDir) {
+  ## the Controls Software folder exists, delete to start install process from scratch
+  log-step -msg $infoBase" D:\Controls Software exists...deleting..."
+  Remove-Item -Path $ctrlDir -Force -Recurse
+
+} else {
+  log-step $infoBase" D:\Controls Software does not exist..."
+
+  if (Test-Path -Path "D:\") {
+    
+      ### D:\ volume exists on disk, create the Controls Software folder.
+      New-Item -Path "D:\" -Name "Controls Software" -ItemType "directory"
+      log-step -msg $infoBase" created directory at "$ctrlDir
+
+      ### Enable audit process tracking local security policy
+      log-step -msg $infoBase" Enabling the audit process tracking local security policy..."
+      audit-process
+
+      ### Copy the installer folder to the proper location
+      <# NOT ENOUGH DISK SPACE IN THE TEST VM !!!!
+      Copy-Item -Path (Get-Location) -Destination $ctrlDir -Recurse -Force
+      #>
+
+      ### Start running install setups
+      foreach ($p in $setupPaths) {
+        run-setup -setupPath $p.FullName -filterExpression *Distech*
+      }
+
+      # variables and constants after install setups
+      $licenseFile = "licenses\Distech.license"
+      $n4Name = ($setupPaths[1].Name -split " ")[($setupPaths[1].Name -split " ").Length - 1]
+      $n4Version = $n4Name.Trim('v', '.', 'e', 'x', 'e')
+      $majorRev = $n4Version.Substring(0,3)
+      $partialLicensePath = "EC-Net4-",$n4Version,"\security\licenses" -join ""
+      $absoluteLicensePath = Join-Path -Path "C:\Niagara" -ChildPath $partialLicensePath
+      $jars = Get-ChildItem -Path .\assets\jars
+      $partialN4JarPath = "EC-Net4-",$n4Version,"\modules" -join ""
+      $absoluteN4JarPath = Join-Path -Path "C:\Niagara" -ChildPath $partialN4JarPath
+      $partialNrePath = "\Niagara",$majorRev,"\distech\etc\nre.properties" -join ""
+      $absoluteNrePath = Join-Path -Path $env:AllUSERSPROFILE -ChildPath $partialNrePath
+      $partialShorcutPath = "Microsoft\Windows\Start\Menu\Programs"
+      $usrShortcutPath = Join-Path -Path $env:APPDATA -ChildPath $partialShorcutPath
+      $allUsrShortcutPath = Join-Path -Path $env:AllUSERSPROFILE -ChildPath $partialShorcutPath
+      $startMenuShortcutFiles = Get-ChildItem -Path $usrShortcutPath
+
+      ## Copy license file to the new N4 license folder in the Niagara install directory
+      Copy-Item -Path $licenseFile -Destination $absoluteLicensePath -Force
+      log-step -msg $infoBase" Copied license file to its proper location in the Niagara license folder..."
+
+      ## Copy Niagara Jar Modules
+      foreach ($jar in $jars) {
+        Copy-Item -Path $jar.FullName -Destination $absoluteN4JarPath -Force
+      }
+      log-step -msg $infoBase" Copied all the jars to their proper location in the Niagara modules folder..."
+
+      ## Edit the nre.properties file
+      Copy-Item -Path $absoluteNrePath -Destination assets -Force
+      Rename-Item -Path assets\nre.properties -NewName "nre_backup.properties"
+      log-step -msg $infoBase" backed-up nre.properties to .\assets\nre_backup.properties"
+
+      log-step -msg $infoBase" scanning nre.properties"
+      Get-Content -Path $absoluteNrePath
+
+      (Get-content -Path $absoluteNrePath).Replace("station.java.options=-Dfile.encoding=UTF-8 -Xss512K -Xmx1024M", "station.java.options=-Dfile.encoding=UTF-8 -Xss512K -Xmx2G") |
+        Out-File $absoluteNrePath
+      
+      Get-Content -Path $absoluteNrePath
+
+      ## Make all the Distech shortchuts available to all users
+      log-step -msg $infoBase" scanning start menu shortcut files"
+      foreach ($f in $startMenuShortcutFiles) {
+        if (($f.FullName).contains("EC-Net")) {
+          Write-Host $f.Name
+          Copy-Item -Path $f.FullName -Destination $allUsrShortcutPath
+        }
+      }
+      
+      log-step -msg $infoBase" copied all shortcut files from the installation user shortcut folder to the all-users one..."
+
+      ## Set the process tracking audit policy back to default
+      audit-process -enable $false
+
+      ## Re-align security execution policy with strict security requirements.
+      Set-ExecutionPolicy Restricted
+
+  } else {
+      log-step $errorBase" The 'D:\' volume does not exist, contact your local controls engineer for assistance"
+  }
+}
